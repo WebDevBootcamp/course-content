9f8bed8a2a380bd33a430838b9bd9c5e5d3ede60
Ben.Vaughan <benjamin.vaughan@gmail.com>
2014-05-06 07:08:46 -0600

    moved 10 to 11
    Moved content of 10 to 11.  Added new 10 in to show simple ajax load
    command.

---
+++ module4/jQuery Exercises/10.Simplified AJAX/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    // Write a simple ajax request here.  Investigate using the shorthand methods found at:
+    // http://api.jquery.com/category/ajax/shorthand-methods/
+
+    $(".target").load("sample.html");
+
+});
